f2e29a452d457beee39cdf8e47305972b688b785
Ankitsaini406 <as.ankitsaini406@gmail.com>
2024-05-20 20:53:13 +0530

    "static side"

---
+++ app/static/page.tsx
@@ -0,0 +1,19 @@
+export default async function StaticSide (){
+    const users = await getUsers();
+
+  return (
+    <div style={{height:'100dvh', padding: '1rem'}}>
+      <h1 style={{padding: '1rem 0rem'}}>User (Static Side Reanding)</h1>
+      {users.data.users.map((user: any) => {
+        return <li key={user.id}>{user.firstName}</li>;
+      })}
+    </div>
+  );
+};
+
+async function getUsers() {
+  const data = await (await fetch("https:/dummyjson.com/users")).json();
+  return {
+      data,
+  };
+};
